6b2cca2d933892eb5edf2546617f21a003fa2c5d
20211054 <115679955+20211054@users.noreply.github.com>
2022-10-13 11:16:11 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,46 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(randint(1, 6))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
